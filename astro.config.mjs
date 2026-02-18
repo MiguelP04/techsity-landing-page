c31db5a9967c2518d71af17e3ec76dbe95b605b0
MiguelP04 <miguel09112004@gmail.com>
2026-02-18 10:54:31 -0400

    astro.config.mjs changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://miguelp04.github.io',
-  base: process.env.NODE_ENV === 'production' ? '/techsity-landing-page/' : '/',
-  trailingSlash: 'always',
-  build: {
-    assets: 'assets'
-  },
   vite: {
     plugins: [tailwindcss()],
   }
